221dc7449d005a893aaffcadcd93ec94aca4085a
Simon Cateau <sorani.dev@putlook.com>
2022-08-28 13:35:40 +0000

    feat: add drafts and publish to posts

--- blog/views.py
+++ blog/views.py
@@ -36,10 +36,22 @@ def post_edit(request: HttpRequest, pk: int):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
 
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request, 'blog/post_draft_list.html', { 'posts': posts })
+
+def post_publish(request: HttpRequest, pk: int):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('post_detail', pk=pk)
+
+def post_remove(request: HttpRequest, pk: int):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('post_list')
